18abe036b6572928ca8072909aef00e8defed0a9
diadasiachilensis <diadasiachilensis@github.com>
2026-09-21 15:50:50 -0300

    Add Weather Observation Sttion 4 finished con count() y distinct

--- basic-select/Weather-Observation-Station-4.sql
+++ basic-select/Weather-Observation-Station-4.sql
@@ -1,2 +1,15 @@
 -- Weather Observation Station 4 -- 
 
+/*
+
+Encontrar la diferencias en el total numero de CITY, y la cantidad de CITY distintos en la tabla
+
+Se usa COUNT(), se encesita la cantidad de ciudades 
+Se usa DISTINCT porque se necesita obtener valores distintos (excluir duplciados), posteriormente
+se debe contar la cantidad con COUNT()
+*/
+
+--seleccionar las columnas y su forma cambiada
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+--Desde donde se obtiene las columnas 
+FROM STATION ; 
